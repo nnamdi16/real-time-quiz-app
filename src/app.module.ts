267a4a58ec73396d70b9bb2973c7c53ff00142d0
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-12-14 21:31:02 +0100

    chore: added ConfigModule to AppModule for reading environment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './db/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
