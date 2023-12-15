d0bb4905b80d1a534ae70b7ec5ee4b1f70650e1b
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-12-15 15:27:33 +0100

    chore: logging configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './db/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
+import { UserModule } from './api/user/user.module';
 
 @Module({
   imports: [
     DatabaseModule,
+    UserModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: 'info',
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
