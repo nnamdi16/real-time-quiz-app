4720075056e598af670949c68905058115801daa
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-12-15 15:32:39 +0100

    chore: refactored database configuration, directly returning the database configuration object

--- src/db/database.module.ts
+++ src/db/database.module.ts
@@ -7,28 +7,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        return {
-          name: 'quiz-app',
-          type: 'postgres',
-          host: configService.get('POSTGRES_HOST'),
-          port: configService.get('POSTGRES_PORT'),
-          username: configService.get('POSTGRES_USER'),
-          password: configService.get('POSTGRES_PASSWORD'),
-          database: configService.get('POSTGRES_DB'),
-          logging: true,
-          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-          synchronize: true,
-          migrationsTableName: 'migration',
-          migrations: ['src/migration/*.ts'],
-          migrationsRun: true,
-          cli: {
-            migrationsDir: 'src/migration',
-          },
-          seeds: ['src/seeds/**/*{.ts,.js}'],
-          factories: ['src/factories/**/*{.ts,.js}'],
-        };
-      },
+      useFactory: (configService: ConfigService) => ({
+        name: 'quiz-app',
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: Number(configService.get('POSTGRES_PORT')),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        logging: true,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+        migrationsTableName: 'migration',
+        migrations: ['src/migration/*.ts'],
+        migrationsRun: true,
+        cli: {
+          migrationsDir: 'src/migration',
+        },
+        seeds: ['src/seeds/**/*{.ts,.js}'],
+        factories: ['src/factories/**/*{.ts,.js}'],
+      }),
     }),
   ],
 })
