e94a25fbc12e1c77299e015011a0de8193c27477
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-12-17 04:56:23 +0100

    feat: implemented login method

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,16 +1,21 @@
 import {
   BadRequestException,
   HttpStatus,
+  Inject,
   Injectable,
   Logger,
+  UnauthorizedException,
 } from '@nestjs/common';
-import { RegisterUserDto } from './user.dto';
+import { LoginDto, RegisterUserDto, TokenData, TokenDto } from './user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { Repository } from 'typeorm';
 import { IResponse, errorHandler } from '../../util/util';
 import * as bcrypt from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { ENV } from '../../constants';
+import EncryptService from '../../util/encryption';
 
 @Injectable()
 export class UserService {
@@ -19,6 +24,9 @@ export class UserService {
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
     private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+    @Inject(EncryptService)
+    private readonly encryptionService: EncryptService,
   ) {}
   //Todo: Handle database error
   async registerUser(payload: RegisterUserDto): Promise<IResponse<UserEntity>> {
@@ -53,5 +61,83 @@ export class UserService {
       this.logger.error(error);
       errorHandler(error);
     }
+  }
+
+  async login(payload: LoginDto): Promise<IResponse<TokenDto>> {
+    try {
+      const { username, password } = payload;
+      const userDetails = await this.userRepository.findOne({
+        where: [{ username }],
+      });
+      if (!userDetails || !password) {
+        throw new UnauthorizedException({
+          statusCode: HttpStatus.UNAUTHORIZED,
+          message: 'Login failed',
+        });
+      }
+
+      const isPasswordMatch = await bcrypt.compare(
+        password,
+        userDetails.password,
+      );
+      if (!isPasswordMatch) {
+        throw new UnauthorizedException({
+          statusCode: HttpStatus.UNAUTHORIZED,
+          message: 'Invalid login',
+        });
+      }
+      const tokenData: TokenData = {
+        id: userDetails.id,
+        email: userDetails.email,
+        username: userDetails.username,
+      };
+
+      const { accessToken, refreshToken } = await this.updateToken(tokenData);
+      return {
+        status: 'success',
+        statusCode: HttpStatus.OK,
+        message: 'Authentication successful',
+        data: { accessToken, refreshToken },
+        error: null,
+      };
+    } catch (error) {
+      this.logger.error(error);
+      errorHandler(error);
+    }
+  }
+
+  async updateToken(
+    tokenData: TokenData,
+  ): Promise<{ accessToken: string; refreshToken: string }> {
+    const { accessToken, refreshToken } = await this.getTokens(tokenData);
+    const encryptRefreshToken =
+      await this.encryptionService.encrypt(refreshToken);
+    await this.userRepository.update(
+      {
+        email: tokenData.email,
+      },
+      { refreshToken: encryptRefreshToken },
+    );
+    return { accessToken, refreshToken };
+  }
+
+  async getTokens(
+    payload: TokenData,
+  ): Promise<{ accessToken: string; refreshToken: string }> {
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(payload, {
+        secret: this.configService.get<string>(ENV.JWT_ACCESS_TOKEN_SECRET),
+        expiresIn: this.configService.get<string>(
+          ENV.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
+        ),
+      }),
+      this.jwtService.signAsync(payload, {
+        secret: this.configService.get<string>(ENV.JWT_REFRESH_TOKEN_SECRET),
+        expiresIn: this.configService.get<string>(
+          ENV.JWT_REFRESH_TOKEN_EXPIRATION_TIME,
+        ),
+      }),
+    ]);
+    return { accessToken, refreshToken };
   }
 }
