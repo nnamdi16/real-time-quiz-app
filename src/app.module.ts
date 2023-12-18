1d5f2755ca5ca2b9e16f2ef9c895f5fcdad33718
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-12-18 23:06:10 +0100

    chore: added quiz module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { DatabaseModule } from './db/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
 import { UserModule } from './api/user/user.module';
+import { QuizModule } from './api/quiz/quiz.module';
 
 @Module({
   imports: [
     DatabaseModule,
     UserModule,
+    QuizModule,
     LoggerModule.forRoot({
       pinoHttp: {
         level: 'info',
