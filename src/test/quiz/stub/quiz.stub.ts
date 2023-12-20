a9f60ec97cf741add306373b796bb5c895ccec7e
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-12-20 21:09:30 +0100

    chore: added more mocked response

--- src/test/quiz/stub/quiz.stub.ts
+++ src/test/quiz/stub/quiz.stub.ts
@@ -66,3 +66,61 @@ export const quiz = {
 export const quizParams: QuizParams = {
   id: '217d86fb-2c0f-46c7-975b-21635e1d7f62',
 };
+
+export const mockedQuestion = {
+  id: 'f448b672-f8e5-42b1-b4d4-1062b9065a68',
+  createdDate: '2023-12-19T00:29:20.497Z',
+  updatedDate: '2023-12-19T00:29:20.497Z',
+  createdBy: null,
+  updatedBy: null,
+  title: 'Find the sum of 1 and 2',
+  quiz: {
+    id: '217d86fb-2c0f-46c7-975b-21635e1d7f62',
+    createdDate: '2023-12-19T00:29:20.497Z',
+    updatedDate: '2023-12-19T00:29:20.497Z',
+    createdBy: null,
+    updatedBy: null,
+    title: 'Maths Quiz',
+  },
+  options: [
+    {
+      id: '53e67897-ba7c-4846-93e1-7f910446d35a',
+      createdDate: '2023-12-19T00:29:20.497Z',
+      updatedDate: '2023-12-19T00:29:20.497Z',
+      createdBy: null,
+      updatedBy: null,
+      text: '3',
+    },
+  ],
+};
+
+export const mockedOngoingQuiz = {
+  status: 'ONGOING',
+  quiz: {
+    id: '217d86fb-2c0f-46c7-975b-21635e1d7f62',
+    createdDate: '2023-12-19T00:29:20.497Z',
+    updatedDate: '2023-12-19T00:29:20.497Z',
+    createdBy: null,
+    updatedBy: null,
+    title: 'Maths Quiz',
+  },
+  user: {
+    id: 'e7e1e3ee-ac78-4a29-b2ad-c323435f065f',
+    email: 'johndoe@example.com',
+    username: 'John#123',
+    iat: 1703093006,
+    exp: 1703096606,
+  },
+  response: [
+    {
+      options: ['53e67897-ba7c-4846-93e1-7f910446d35a'],
+      question: 'f448b672-f8e5-42b1-b4d4-1062b9065a68',
+    },
+  ],
+  score: 1,
+  createdBy: null,
+  updatedBy: null,
+  id: '1e9ff003-a97d-423a-853d-07fce03a94f9',
+  createdDate: '2023-12-20T17:23:51.780Z',
+  updatedDate: '2023-12-20T17:23:51.780Z',
+};
