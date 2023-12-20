7acab62208a2e79df268528d24b0c95ff80fa19d
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-12-20 21:06:55 +0100

    feat: Implemented getQuiz, submitAnswer and getScore method

--- src/api/quiz/quiz.service.ts
+++ src/api/quiz/quiz.service.ts
@@ -4,17 +4,20 @@ import {
   Inject,
   Injectable,
   Logger,
+  NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ILike, Repository } from 'typeorm';
 import { IResponse, errorHandler, handlePagination } from '../../util/util';
 import { Quiz } from './quiz.entity';
-import { QuizDto } from './quiz.dto';
+import { QuizDto, TestStatus, UserResponse, UserResponseDto } from './quiz.dto';
 import { TokenData } from '../user/user.dto';
 import { UserService } from '../user/user.service';
 import { Pagination } from '../shared/pagination.dto';
 import { UUID } from 'crypto';
+import { Question } from './questions.entity';
+import { Results } from './results.entity';
 
 @Injectable()
 export class QuizService {
@@ -22,8 +25,14 @@ export class QuizService {
   constructor(
     @InjectRepository(Quiz)
     private readonly quizRepository: Repository<Quiz>,
+    @InjectRepository(Question)
+    private readonly questionRespository: Repository<Question>,
+    @InjectRepository(Results)
+    private readonly resultRepository: Repository<Results>,
     @Inject(UserService)
     private readonly userService: UserService,
+    // @Inject(EventBusService)
+    // private readonly eventService: EventBusService,
   ) {}
 
   async create(
@@ -93,12 +102,132 @@ export class QuizService {
         where: { id },
         relations: ['questions', 'questions.options'],
       });
-
       return {
         status: 'success',
         statusCode: HttpStatus.OK,
         message: 'Quiz fetched successfully',
         data: data,
+        error: null,
+      };
+    } catch (error) {
+      this.logger.error(error);
+      errorHandler(error);
+    }
+  }
+  async joinQuiz(id: UUID): Promise<IResponse<Quiz>> {
+    try {
+      const data = await this.quizRepository.findOne({
+        where: { id },
+        relations: ['questions', 'questions.options'],
+      });
+      return {
+        status: 'success',
+        statusCode: HttpStatus.OK,
+        message: 'Successfully joined a quiz',
+        data,
+        error: null,
+      };
+    } catch (error) {
+      this.logger.error(error);
+      errorHandler(error);
+    }
+  }
+  async submitAnswer(
+    id: UUID,
+    { options }: UserResponseDto,
+    user: TokenData,
+  ): Promise<IResponse<Question>> {
+    try {
+      const data = await this.questionRespository
+        .createQueryBuilder('questions')
+        .innerJoinAndSelect('questions.quiz', 'quiz')
+        .innerJoinAndSelect(
+          'questions.options',
+          'options',
+          'options.isCorrect = :isCorrect',
+          { isCorrect: true },
+        )
+        .where('questions.id = :id', { id })
+        .getOne();
+
+      if (!data?.options.length) {
+        throw new NotFoundException({
+          statusCode: HttpStatus.NOT_FOUND,
+          message: 'Question Unavailable',
+        });
+      }
+      const ongoingQuiz = await this.resultRepository.findOne({
+        where: {
+          user: {
+            id: user.id,
+          },
+          quiz: {
+            id: data.quiz.id,
+          },
+          status: TestStatus.ONGOING,
+        },
+      });
+
+      const compareUserAnswer = data.options.every((option) =>
+        options.includes(option.id),
+      );
+      const userResponse: UserResponse = { question: data.id, options };
+
+      if (!ongoingQuiz) {
+        await this.resultRepository.save({
+          status: TestStatus.ONGOING,
+          quiz: data.quiz,
+          user,
+          response: [userResponse],
+          score: compareUserAnswer ? 1 : 0,
+        });
+      } else {
+        await this.resultRepository
+          .createQueryBuilder()
+          .update(Results)
+          .set({
+            score: () => `score + ${compareUserAnswer ? 1 : 0}`,
+            response: [...ongoingQuiz.response, userResponse],
+          })
+          .where('id = :id', { id: ongoingQuiz.id })
+          .execute();
+      }
+      const responseMessage = compareUserAnswer ? 'correct' : 'incorrect';
+      return {
+        status: 'success',
+        statusCode: HttpStatus.OK,
+        message: `The provided answer is ${responseMessage}`,
+        data: null,
+        error: null,
+      };
+    } catch (error) {
+      this.logger.error(error);
+      errorHandler(error);
+    }
+  }
+
+  async getScore(id: UUID, user: TokenData): Promise<IResponse<Results>> {
+    try {
+      const data = await this.resultRepository
+        .createQueryBuilder('result')
+        .leftJoinAndSelect('result.user', 'user')
+        .leftJoinAndSelect('result.quiz', 'quiz')
+        .where('user.id = :userId', { userId: user.id })
+        .andWhere('quiz.id = :quizId', { quizId: id })
+        .andWhere('result.status = :status', { status: TestStatus.ONGOING })
+        .select(['result.score'])
+        .getOne();
+      if (!data) {
+        throw new NotFoundException({
+          statusCode: HttpStatus.NOT_FOUND,
+          message: 'Score not found',
+        });
+      }
+      return {
+        status: 'success',
+        statusCode: HttpStatus.OK,
+        message: 'User score retrieved successfully',
+        data,
         error: null,
       };
     } catch (error) {
