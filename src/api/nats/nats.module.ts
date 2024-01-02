757dc6b1e90aa3cb65421a1a78363e7410d025f7
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-01-03 00:34:55 +0100

    chore: added AppGateway

--- src/api/nats/nats.module.ts
+++ src/api/nats/nats.module.ts
@@ -1,9 +1,9 @@
 import { Module, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { NatService } from './nats.service';
-import { WebsocketGateway } from '../quiz/quiz.gateway';
+import { AppGateway } from '../../app.gateway';
 
 @Module({
-  providers: [NatService, WebsocketGateway],
+  providers: [NatService, AppGateway],
   exports: [NatService],
 })
 export class NatsModule implements OnModuleInit, OnModuleDestroy {
